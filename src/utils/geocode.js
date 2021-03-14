6182a58d2ba182f3d68939466d35ee06a2b7250a
unknown <raymondtant@gmail.com>
2021-03-14 09:03:26 -0500

    saved again

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('postman-request')
 
 const geocode = (address, geocode_key, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token='+ geocode_key 
-
+console.log(url)
     request({url:url, json: true}, (error, {body}) => {
         if (error) {
             callback('Unable to connect to location services', undefined)
